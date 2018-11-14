2eb13c71b856ac409d5862d01b0200c218098f58
arkhamanalysis7638 <45046157+arkhamanalysis7638@users.noreply.github.com>
2018-11-14 23:42:46 +0530

    Add files via upload

---
+++ automatic_water_pumping.ino
@@ -0,0 +1,22 @@
+int ACWATERPUMP = 13;
+int SENSOR = 8;
+int val;
+void setup() {
+  pinMode(13,OUTPUT);
+  pinMode(8,INPUT);
+
+}
+
+void loop() {
+  val = digitalRead(8);
+  if(val == LOW)
+  {
+    digitalWrite(13,LOW);
+  }
+  else
+  {
+    digitalWrite(13,HIGH);
+  }
+  delay(400);
+
+}
